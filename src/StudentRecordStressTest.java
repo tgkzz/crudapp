89be4c8808c99a2400ee8cd05d4d015f3e5b9027
tgkzz <lilkamavertttt@gmail.com>
2023-09-20 19:26:17 +0600

    changed stress test

--- src/StudentRecordStressTest.java
+++ src/StudentRecordStressTest.java
@@ -1,36 +1,58 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Random;
+import java.util.Scanner;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.junit.Assert.*;
+
 public class StudentRecordStressTest {
-    // In the result of the test, it should give no panic, and complete as fast as possible
-    public static void main(String[] args) {
-        StudentRecordManagement stress = new StudentRecordManagement();
+    StudentRecordManagement management;
+    Random random;
 
-        Record record = new Record();
+    @Before
+    public void setUp() {
+        management = new StudentRecordManagement();
+        random = new Random();
+    }
 
-        // test for high volume of records
-        for (int i = 1; i < 10000; i++) {
-            // add data into the linked list
-            record = new Record("test-name", i, i);
-            stress.add(record);
+    @Test
+    public void stressTest() {
+        // add 10000 records
+        for (int i = 1; i <= 10000; i++) {
+            Record r = new Record("Student" + i, i, random.nextInt(10000));
+            management.add(r);
         }
 
-        // test for display a big range of elements
-        stress.display();
-
-        // test for updating all elements of elements into 1 name
-        for (int i = 1; i < 10000; i++) {
-            if(stress.find(i)) {
-                Record rec = stress.findRecord(i);
-                rec.setIdNumber(i++);
-                rec.setContactNumber(i++);
-                rec.setName("test-name-for-crud");
-            }
+        // check if all 10000 values are correctly outputted
+        for (int i = 1; i <= 10000; i++) {
+            assertTrue(management.find(i));
         }
-        stress.display();
 
-        // test for deleting all record
-        for (int i = 1; i < 10000; i++){
-            stress.delete(i);
+        // update 10000 records
+        for (int i = 1; i <= 10000; i++) {
+            String updateInput = String.format("%d\n%d\n%s\n", i + 10000, random.nextInt(10000), "UpdatedStudent" + i);
+            InputStream in = new ByteArrayInputStream(updateInput.getBytes());
+            Scanner scanner = new Scanner(in);
+            management.update(i, scanner);
         }
-        stress.display();
 
+        // check if all 10000 records where updated
+        for (int i = 10001; i <= 20000; i++) {
+            assertTrue(management.find(i));
+        }
+
+
+        // delete all 10000 records
+        for (int i = 10001; i <= 20000; i++) {
+            management.delete(i);
+        }
+
+        // check if all 10000 records were deleted
+        for (int i = 10001; i <= 20000; i++) {
+            assertFalse(management.find(i));
+        }
     }
 }
